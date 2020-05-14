b26af08e1ffe8c57ebb82b7b3c2f549f8eb5ec1e
siripong <aeio.yu@gmail.com>
2020-05-14 13:23:23 +0700

    initial

---
+++ precache-manifest.3e483a06b362f631e5f6567a3188c021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef707efc97624f4d5772be8e43f590d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aecd22c5cf5a70631bc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b14cbff3c29fb55de8eb",
+    "url": "/static/js/2.a4a81ece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4a81ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aecd22c5cf5a70631bc",
+    "url": "/static/js/main.2703eb3e.chunk.js"
+  },
+  {
+    "revision": "d8eced3c2e13c847380d",
+    "url": "/static/js/runtime-main.4af9d08a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
